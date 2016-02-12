6eb9d4a6d4d7f112471753d32813f9a7bd035ddb
charlieperson <charlie.person@hyperisland.se>
2016-02-12 11:50:46 +0000

    Adds another controller test

--- test/jamminController.spec.js
+++ test/jamminController.spec.js
@@ -1,16 +1,34 @@
 describe('JamminController', function() {
-
-  var ctrl;
-
   beforeEach(module('Jammin'));
 
-  beforeEach(inject(function($controller, $rootScope){
-    var scope = $rootScope.$new();
-    ctrl = $controller('JamminController', {$scope: scope});
-  }));
-
   it('initializes statusLabel as not connected', function() {
+    var ctrl;
+    inject(function($controller, $rootScope){
+      var scope = $rootScope.$new();
+      ctrl = $controller('JamminController', {$scope: scope});
+    });
+
     expect(ctrl.statusLabel).toEqual('not connected');
   });
 
+
+  describe('when visiting the page', function() {
+    beforeEach(function() {
+      module(function ($provide) {
+        fakeSockets = jasmine.createSpyObj('fakeSockets', ['setup']);
+        $provide.factory('SocketFactory', function() {
+          return fakeSockets;
+        })
+      })
+    });
+
+    beforeEach(inject(function($controller, $rootScope) {
+      var scope = $rootScope.$new();
+      ctrl = $controller('JamminController', {$scope: scope});
+    }));
+
+    it('sets up sockets', function() {
+      expect(fakeSockets.setup).toHaveBeenCalled();
+    });
+  });
 });
