803c5b37e25dc239037816d7a589f2a94389ee98
Daniela <daniela.galeote@gmail.com>
2016-02-14 17:21:08 +0000

    Linking sound to keypress

---
+++ app/js/factories/keyboardFactory.js
@@ -0,0 +1,31 @@
+jammin.factory('KeyboardFactory', [function() {
+  var keyboardFactory = {};
+
+  keyboardFactory.keypress = function(keyEvent,callback) {
+    var key = keyEvent.which;
+    if (key === 99) { callback("self.playSound('C4')"); }
+    else if (key === 100) { callback("self.playSound('D4')"); }
+    //   break;
+      // case 101:
+      //   callback("self.playSound('E4')");
+      //   break;
+      // case 102:
+      //   callback("self.playSound('F4')");
+      //   break ;
+      // case 103:
+      //   callback("self.playSound('G4')");
+      //   break;
+      // case 97:
+      //   callback("self.playSound('A4')");
+      //   break;
+      // case 98:
+      //   callback("self.playSound('B4')");
+      //   break;
+
+  }
+
+
+
+  return keyboardFactory;
+
+}]);
