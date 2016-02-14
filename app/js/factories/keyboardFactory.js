f5e1be1d94a50c21878ca71b0242132a8cc92b15
hibreez <4ngus.whitehead@gmail.com>
2016-02-14 17:56:08 +0000

    Moves ng-keypress to keyboard div to avoid conflicts and use if..else over switch.

--- app/js/factories/keyboardFactory.js
+++ app/js/factories/keyboardFactory.js
@@ -3,31 +3,29 @@ jammin.factory('KeyboardFactory', [function() {
 
   keyboardFactory.keypress = function(keyEvent,callback) {
     var key = keyEvent.which;
-    switch(key){
-      case 99:
-        callback("self.playSound('C4')");
-        break;
-      case 100:
-        callback("self.playSound('D4')");
-        break;
-      case 101:
-        callback("self.playSound('E4')");
-        break;
-      case 102:
-        callback("self.playSound('F4')");
-        break;
-      case 103:
-        callback("self.playSound('G4')");
-        break;
-      case 97:
-        callback("self.playSound('A4')");
-        break;
-      case 98:
-        callback("self.playSound('B4')");
-        break;
-
-      default:
-        callback('console.log(keyEvent.which)');
+    if (key ===  99 ) {
+      callback("self.playSound('C4')");
+    }
+    else if (key ===  100 ) {
+      callback("self.playSound('D4')");
+    }
+    else if (key ===  101 ) {
+      callback("self.playSound('E4')");
+    }
+    else if (key ===  102 ) {
+      callback("self.playSound('F4')");
+    }
+    else if (key ===  103 ) {
+      callback("self.playSound('G4')");
+    }
+    else if (key ===  97 ) {
+      callback("self.playSound('A4')");
+    }
+    else if (key ===  98 ) {
+      callback("self.playSound('B4')");
+    }
+    else {
+      callback('console.log(keyEvent.which)');
     }
   };
 
