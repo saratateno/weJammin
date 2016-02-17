cf6ba31fe5b2721c9ef40e134038a9c77525a821
Daniela <daniela.galeote@gmail.com>
2016-02-17 09:49:25 +0000

    White keys on keypress

--- app/js/factories/keyboardFactory.js
+++ app/js/factories/keyboardFactory.js
@@ -9,20 +9,29 @@ jammin.factory('KeyboardFactory', [function() {
     else if (key ===  120 ) {
       callback("self.playSound('D3')");
     }
-    else if (key ===  99 ) {
+    else if (key === 99 ) {
       callback("self.playSound('E3')");
     }
-    else if (key ===  118 ) {
+    else if (key === 118 ) {
       callback("self.playSound('F3')");
     }
-    else if (key ===  98 ) {
+    else if (key === 98 ) {
       callback("self.playSound('G3')");
     }
-    else if (key ===  110 ) {
+    else if (key === 110 ) {
       callback("self.playSound('A3')");
     }
-    else if (key ===  109 ) {
+    else if (key === 109 ) {
       callback("self.playSound('B3')");
+    }
+    else if (key === 44 ) {
+      callback("self.playSound('C4')");
+    }
+    else if (key === 46 ) {
+      callback("self.playSound('D4')");
+    }
+    else if (key === 47 ) {
+      callback("self.playSound('E4')");
     }
     else {
       callback('console.log(keyEvent.which)');
