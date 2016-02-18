a46fd256a1f33434c5ffd21fa7eaf89f62667ff3
Daniela <daniela.galeote@gmail.com>
2016-02-18 15:05:42 +0000

    Pulling development

--- app/js/keyboardController.js
+++ app/js/keyboardController.js
@@ -1,15 +1,15 @@
-keyboard.controller('KeyboardController',
-  ['SocketFactory', 'SoundFactory', 'UserFactory',
-  'KeyboardFactory', 'DrumFactory', 'TransportFactory',
-  function(SocketFactory, SoundFactory, UserFactory,
-    KeyboardFactory, DrumFactory, TransportFactory) {
-
-
-  var self = this;
-
-  self.keypress = function(keyEvent) {
-    KeyboardFactory.keypress(keyEvent, function(action) {
-      eval(action);
-    });
-  }
-}]);
+// keyboard.controller('KeyboardController',
+//   ['SocketFactory', 'SoundFactory', 'UserFactory',
+//   'KeyboardFactory', 'DrumFactory', 'TransportFactory',
+//   function(SocketFactory, SoundFactory, UserFactory,
+//     KeyboardFactory, DrumFactory, TransportFactory) {
+//
+// 
+//   var self = this;
+//
+//   self.keypress = function(keyEvent) {
+//     KeyboardFactory.keypress(keyEvent, function(action) {
+//       eval(action);
+//     });
+//   }
+// }]);
